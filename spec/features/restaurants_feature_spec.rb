e6f9489a6e03d389b0e8566ba342534906928b74
Nickrhys <nickrhys@hotmail.com>
2014-09-29 12:48:29 +0100

    display restaurants test passed

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,17 @@ describe 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurants have been added' do
+    before do 
+      Restaurant.create(name: 'Pizza Pilgrims')
+    end
+    
+    it 'should display restaurants' do  
+      visit '/restaurants' 
+        expect(page).to have_content('Pizza Pilgrims')
+        expect(page).not_to have_content('No restaurants yet')
+      end
+    end 
 end
+
