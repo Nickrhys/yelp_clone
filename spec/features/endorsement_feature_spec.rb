af7626d7a59071bccd666bd05ffa4d02bb9e1d88
Nickrhys <nickrhys@hotmail.com>
2014-10-02 11:26:03 +0100

    added and tested pluralize for endorsements

--- spec/features/endorsement_feature_spec.rb
+++ spec/features/endorsement_feature_spec.rb
@@ -12,4 +12,11 @@ describe 'endorsing reviews' do
 		expect(page).to have_content '1 endorsement'
 	end
 
+	it "will pluralize the number of endorsements when it is greater than 1", js: true do 
+		visit '/restaurants'
+		click_link 'Endorse this review'
+		click_link 'Endorse this review'
+		expect(page).to have_content '2 endorsements'
+	end
+
 end
